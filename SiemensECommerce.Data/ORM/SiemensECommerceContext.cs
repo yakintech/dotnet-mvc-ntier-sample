b344586e58f2972678a120fdd7f4c110b98d0d61
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2022-06-20 09:57:20 +0300

    project init

---
+++ SiemensECommerce.Data/ORM/SiemensECommerceContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiemensECommerce.Data.ORM
+{
+    public class SiemensECommerceContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS; Database=SiemensECommerce; trusted_connection = true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ SiemensECommerce.UI/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiemensECommerce.Data.ORM;
+
+namespace SiemensECommerce.UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            SiemensECommerceContext db = new SiemensECommerceContext();
+
+            var categories = db.Categories.ToList();
+            return View();
+        }
+    }
+}

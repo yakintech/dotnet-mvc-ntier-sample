6b9ceac9f8f725f52a4edd66a06c89eb323f3634
Emre <seantolstoyevski@protonmail.com>
2022-06-23 11:49:11 +0300

    Fix Namwe -> Name in WebUser

--- SiemensECommerce.Data/ORM/WebUser.cs
+++ SiemensECommerce.Data/ORM/WebUser.cs
@@ -2,7 +2,7 @@ namespace SiemensECommerce.Data.ORM;
 
 public class WebUser : BaseEntity
 {
-    public string Namwe { get; set; }
+    public string Name { get; set; }
     public string SurName { get; set; }
     public string Email {  get; set; }
     public string Password { get; set; }

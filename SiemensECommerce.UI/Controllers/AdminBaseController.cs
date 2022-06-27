d76a1cea1601cd073c86148b99804fe48990a7e6
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2022-06-27 09:42:45 +0300

    adminuser login operation

--- SiemensECommerce.UI/Controllers/AdminBaseController.cs
+++ SiemensECommerce.UI/Controllers/AdminBaseController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
 
 namespace SiemensECommerce.UI.Controllers
 {
     [Authorize]
     public class AdminBaseController : Controller
     {
-       
+        //Bu metot herhangi bir action çalışmadan önce ÇALIŞACAKTIR.
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var userEmail = HttpContext.User.Claims.FirstOrDefault(q => q.Type == ClaimTypes.Email)?.Value;
+            ViewBag.email = userEmail;
+        }
     }
 }

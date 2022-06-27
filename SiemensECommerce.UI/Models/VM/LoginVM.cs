9f9efeabf31342674624822ce45787877bd0af5d
afrikalileo <veyseltekin1683@gmail.com>
2022-06-27 13:37:52 +0300

    login validation done

---
+++ SiemensECommerce.UI/Models/VM/LoginVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace SiemensECommerce.UI.Models.VM
+{
+    public class LoginVM
+    {
+
+        [Required(ErrorMessage = "EMail alanı boş bırakılamaz")]
+        [EmailAddress(ErrorMessage = "Lütfen email formatına uygun bir değer giriniz")]
+        public string email { get; set; }
+
+
+
+        [Required(ErrorMessage = "Password alanı boş bırakılamaz")]
+        [MinLength(6, ErrorMessage = "Password minimum 6 karakter olmalıdır")]
+        public string password { get; set; }
+    }
+}

14f2993cde740c81ff0e9bc737d80f7570257b85
Dilek Eda Gündüz <36606579+DilekEdagunduz@users.noreply.github.com>
2022-07-06 11:03:16 +0300

    güncelleme

---
+++ SiemensECommerce.UI/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiemensECommerce.UI.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

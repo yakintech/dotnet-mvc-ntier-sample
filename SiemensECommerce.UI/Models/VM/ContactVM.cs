8f93ffcdbe65de7c1929ad8c277c6c0c69d8d47f
Dilek Eda Gündüz <36606579+DilekEdagunduz@users.noreply.github.com>
2022-07-06 11:26:39 +0300

    Contact sayfası baslangic

---
+++ SiemensECommerce.UI/Models/VM/ContactVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SiemensECommerce.UI.Models.VM
+{
+    public class ContactVM
+    {
+        [Required(ErrorMessage = "Name alanı boş bırakılamaz")]
+
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Surname alanı boş bırakılamaz")]
+
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Phone alanı boş bırakılamaz")]
+
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Message alanı boş bırakılamaz")]
+
+        public string Message { get; set; }
+    }
+}

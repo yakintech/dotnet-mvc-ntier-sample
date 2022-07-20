d99892d7d439bf1adfe80f278b650b6f439fb76f
afrikalileo <veyseltekin1683@gmail.com>
2022-07-20 15:51:16 +0300

    adminusercategory is done

---
+++ SiemensECommerce.UI/Models/VM/CategoryUpdateVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace SiemensECommerce.UI.Models.VM
+{
+    public class CategoryUpdateVM
+    {
+        [Required(ErrorMessage = "Kategori adı boş bırakılamaz boş bırakılamaz")]
+        public string Name { get; set; }
+
+        
+    }
+}
